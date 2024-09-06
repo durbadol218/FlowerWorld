cef03ba45acb45c45dffa8595f9025ff62ad434a
Durbadol Goswami <goswamidurbadol@gmail.com>
2024-09-06 20:14:56 +0600

    Updated settings.py

--- FlowerWorld/settings.py
+++ FlowerWorld/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,13 +65,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
+    "https://flower-world-frontend.vercel.app"
 ]
-CSRF_TRUSTED_ORIGINS = ['https://flowerworld.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://flowerworld.onrender.com','https://flower-world-frontend.vercel.app','https://*.127.0.0.1',]
 
 ROOT_URLCONF = 'FlowerWorld.urls'
 
