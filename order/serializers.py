cce2228bf1de1d8c943a0f2b4849cefd8cf10cb1
Durbadol Goswami <goswamidurbadol@gmail.com>
2024-09-11 11:27:19 +0600

    Updated order.serializers

--- order/serializers.py
+++ order/serializers.py
@@ -3,12 +3,16 @@ from .models import Order
 from flowers.models import Flower
 
 class OrderSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
     flower_name = serializers.SerializerMethodField()
     flower_price = serializers.SerializerMethodField()
     
     class Meta:
         model = Order
-        fields = ['id', 'user', 'placed_time', 'status', 'flower', 'flower_name', 'flower_price', 'quantity', 'total_amount']
+        fields = ['id', 'username', 'placed_time', 'status', 'flower', 'flower_name', 'flower_price', 'quantity', 'total_amount']
+    
+    def get_username(self, obj):
+        return obj.user.user.username if obj.user else None
     
     def get_flower_name(self, obj):
         return obj.flower.flower_name if obj.flower else None
